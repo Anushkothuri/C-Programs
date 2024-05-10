d0f5e3fe7273142bb76ef20d378ede89315cfdb5
Anushkothuri <kothurianush16@gmail.com>
2024-05-10 13:53:44 +0530

    sum

--- problem1.c
+++ problem1.c
@@ -1,2 +1,18 @@
-// C Program to calculate factorial of input number given by user.
-//Using Functions.
+// C Program to calculate sum of input numbers given by user.
+//Using Functions. 
+#include<stdio.h>
+int sum(int,int);
+int main()
+{
+    int a,b;
+    printf("enter a and b:");
+    scanf("%d%d",&a,&b);
+    sum(a,b);
+    return 0;
+}
+int sum(int x,int y)
+{
+    int res=x+y;
+    printf("sum is %d.",res);
+    return 0;
+}
