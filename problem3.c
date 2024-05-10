221be88c19e004618eafe3ead48daf180fdce016
Anushkothuri <kothurianush16@gmail.com>
2024-05-10 14:16:56 +0530

    prime

---
+++ problem3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int Prime(int);
+int main()
+{
+    int n;
+    printf("enter n value:");
+    scanf("%d",&n);
+    Prime(n);
+    return 0;
+}
+int Prime(int n)
+{
+    if(n%1==0&&n%n!=0)
+    printf("%d is a Prime number.",n);
+    else
+    printf("%d is not a Prime number.",n);
+    return 0;
+}
