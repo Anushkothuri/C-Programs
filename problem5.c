35d0e15526ef52b0033417a1d69b4258bb36a252
Anushkothuri <kothurianush16@gmail.com>
2024-05-17 19:14:59 +0530

    maxnum

---
+++ problem5.c
@@ -0,0 +1,41 @@
+// Maximum of 4 numbers.
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter four numbers:");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            if(a>d)
+            {
+                printf("%d is greater",a);
+            }
+        }
+    }
+    else
+    {
+        if(b>c)
+        {
+            if(b>d)
+            {
+                printf("%d is greater",b);
+            }
+        }
+    }
+    if(c>d)
+    {
+        printf("%d is greater",c);
+    }
+    else
+    {
+        printf("%d is greater",d);
+    }
+    
+    
+    return 0;
+   
+
+}
