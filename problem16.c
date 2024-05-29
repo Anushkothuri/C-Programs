6df4c2db6c94737557943c9f8481a79d3c78f4b1
Anushkothuri <kothurianush16@gmail.com>
2024-05-29 09:35:55 +0530

    loops

---
+++ problem16.c
@@ -0,0 +1,11 @@
+// Using loop concept print numbers from 1 to 10.
+#include<stdio.h>
+int main()
+{
+    int n=10;
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d\t",i);
+    }
+    return 0;
+}
