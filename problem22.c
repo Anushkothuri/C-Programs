e27457336cfa9a3861d464a67e8cc0fe30acecfa
Anushkothuri <kothurianush16@gmail.com>
2024-06-03 20:41:50 +0530

    circle

---
+++ problem22.c
@@ -0,0 +1,28 @@
+// find diameter,area,circumference of a circle.
+#include<stdio.h>
+#include<math.h>
+void area(int);
+void circumference(int);
+int main()
+{
+    int r;
+    printf("enter radius: ");
+    scanf("%d",&r);
+    area(r);
+    circumference(r);
+    return 0;
+}
+void area(int x)
+{
+    float area;
+    area=3.14*x*x;
+    printf("area is %f.\n",area);
+}
+void circumference(int c)
+{
+    float circumference=2*3.14*c;
+    printf("circumference of circle is %f.\n",circumference);
+    int diameter;
+    diameter=c+c;
+    printf("diameter of circle is %d.",diameter);
+}
