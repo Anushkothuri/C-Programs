a3f8ca1f6079806de97a0469a3264b729e70ae2e
Anushkothuri <kothurianush16@gmail.com>
2024-06-03 20:48:00 +0530

    do-while

---
+++ problem18.c
@@ -0,0 +1,13 @@
+// Using do while loop print 1 to 10 numbers.
+#include<stdio.h>
+int main()
+{
+    int n=10;
+    int i=1;
+    do
+    {
+        printf("%d\t",i);
+        i++;
+    }while(i<=n);
+    return 0;
+}
